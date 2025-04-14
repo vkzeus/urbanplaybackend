617bef92cbe837ab82f5a108dbb706c99aa5c26a
vkzeus <150108950+vkzeus@users.noreply.github.com>
2025-04-14 15:01:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 // Enable CORS
-app.use(cors({ origin: "https://your-frontend.vercel.app" }));
+app.use(cors());
 
 
 app.use(express.json());
